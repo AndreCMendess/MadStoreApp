f3be2d3ca7258c234796585daac859002bbd9fda
AndreCMendess <andremendesdede2@gmail.com>
2024-11-04 11:39:18 -0300

    criando tela de login e validacao do input de email e senha

--- src/javaScripts/script.js
+++ src/javaScripts/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+    
     $('#mobile_btn').on('click',function(){
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find('i').toggleClass('fa-x');
@@ -58,5 +58,63 @@ $(document).ready(function(){
         distance:'20%'
 
     });
+
+
+
+     //Atalho crtl + shift + l para tela de login aparecer
+     $(document).on('keydown',function(event){
+        if(event.ctrlKey && event.shiftKey && event.key.toLowerCase() === 'l'){
+            event.preventDefault();
+            $('#login').toggle();
+           
+        }
+
+    });
+
+
+    //validar dados de entrada de login
+    $.validator.addMethod("senhaSegura",function(value,element){
+        return this.optional(element) || /^(?=.*[A-Za-z])(?=.*\d).+$/.test(value);
+    }, " A senha deve conter pelomenos uma letra e um numero");
+
+    $('#login_form').validate({
+        rules:{
+            email:{
+                required:true,
+                email:true
+            },
+            senha:{
+                required:true,
+                senhaSegura:true,
+                minlength:6
+
+            }
+        },
+        messages:{
+            email:{
+                required:"O email é obrigatorio para autenticação",
+                email:"O email deve ser valido"
+            },
+            senha:{
+                required:"Por favor ensira sua senha",
+                minlength:"A senha deve conter pelomenos 6 caracteres"
+            }
+        },errorPlacement: function(error,element){
+            error.appendTo(element.parent().find(".error"));
+        }
+    });
+
+    //validar login
+    $('#btn_login').on('click',function(event){
+        let email = $("[name='email']").val();
+        let password = $("[name='senha']").val();
+        
+
+        if($('#login_form').valid()){
+            console.log(email);
+            console.log(password);
+        }
+    });
+
 
 });
