5cc7e5d0cf9994232879e45a0c31adcbfa76bac7
AndreCMendess <andremendesdede2@gmail.com>
2024-12-21 17:40:09 -0300

    adicionando getter e setters e alterando metodo findAll para retornar uma ResponseEntity

--- loja/src/main/java/com/madstore/loja/controller/ProdutoController.java
+++ loja/src/main/java/com/madstore/loja/controller/ProdutoController.java
@@ -3,6 +3,7 @@ package com.madstore.loja.controller;
 import com.madstore.loja.DTO.ProdutoDTO;
 import com.madstore.loja.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ public class ProdutoController {
     @Autowired
     ProdutoService produtoService;
 
-    public List<ProdutoDTO> findAll(){
-        return produtoService.findAll();
+    public ResponseEntity<List<ProdutoDTO>> findAll(){
+        return ResponseEntity.ok(produtoService.findAll());
     }
 }
--- loja/src/main/java/com/madstore/loja/model/Produto.java
+++ loja/src/main/java/com/madstore/loja/model/Produto.java
@@ -16,5 +16,43 @@ public class Produto {
     private String tamanho;
     private Double valor;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
 }
