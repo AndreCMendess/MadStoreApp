83482c9bed5fcb1bb3f318787942615cbfecf34c
AndreCMendess <andremendesdede2@gmail.com>
2024-12-22 16:02:46 -0300

    adiciona getters e setters para produtoDTO

--- loja/src/main/java/com/madstore/loja/DTO/ProdutoDTO.java
+++ loja/src/main/java/com/madstore/loja/DTO/ProdutoDTO.java
@@ -18,4 +18,44 @@ public class ProdutoDTO {
                 this.tamanho = produto.getTamanho();
                 this.valor = produto.getValor();
         }
+
+        public Long getId() {
+                return id;
+        }
+
+        public void setId(Long id) {
+                this.id = id;
+        }
+
+        public String getNome() {
+                return nome;
+        }
+
+        public void setNome(String nome) {
+                this.nome = nome;
+        }
+
+        public String getDescricao() {
+                return descricao;
+        }
+
+        public void setDescricao(String descricao) {
+                this.descricao = descricao;
+        }
+
+        public String getTamanho() {
+                return tamanho;
+        }
+
+        public void setTamanho(String tamanho) {
+                this.tamanho = tamanho;
+        }
+
+        public Double getValor() {
+                return valor;
+        }
+
+        public void setValor(Double valor) {
+                this.valor = valor;
+        }
 }
