077fa93be19b842cec27aba3d553087ec6ba8568
AndreCMendess <andremendesdede2@gmail.com>
2024-12-22 15:52:49 -0300

    importa dependencia thymeleaf substitui hrefs estaticos por th:href/@{} para compatibilidade com Thymeleaf adicionando view controller para renderizar html

--- loja/src/main/java/com/madstore/loja/controller/ProdutoController.java
+++ loja/src/main/java/com/madstore/loja/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import com.madstore.loja.DTO.ProdutoDTO;
 import com.madstore.loja.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class ProdutoController {
     @Autowired
     ProdutoService produtoService;
 
+    @GetMapping
     public ResponseEntity<List<ProdutoDTO>> findAll(){
         return ResponseEntity.ok(produtoService.findAll());
     }
