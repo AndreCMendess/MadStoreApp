e5808e78b042c1bc86b82cafa96621cb831d75a4
AndreCMendess <andremendesdede2@gmail.com>
2024-12-22 18:05:52 -0300

    implemento de metodos no service e controller de POST, DELETE , CREATE e findById

--- loja/src/main/java/com/madstore/loja/controller/ProdutoController.java
+++ loja/src/main/java/com/madstore/loja/controller/ProdutoController.java
@@ -1,12 +1,11 @@
 package com.madstore.loja.controller;
 
 import com.madstore.loja.DTO.ProdutoDTO;
+import com.madstore.loja.model.Produto;
 import com.madstore.loja.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +18,25 @@ public class ProdutoController {
     @GetMapping
     public ResponseEntity<List<ProdutoDTO>> findAll(){
         return ResponseEntity.ok(produtoService.findAll());
+    }
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<Produto> findById(@PathVariable Long id){
+        return ResponseEntity.ok(produtoService.findById(id));
+    }
+
+    @PutMapping(value="/{id}")
+    public ResponseEntity<ProdutoDTO> atualizarProduto(@PathVariable Long id, @RequestBody ProdutoDTO produtoAtualizado){
+        return ResponseEntity.ok(produtoService.atualizarProduto(id,produtoAtualizado));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoDTO> adicionarProduto(@RequestBody ProdutoDTO novoProduto){
+        return ResponseEntity.ok(produtoService.adicionarProduto(novoProduto));
+    }
+
+    @DeleteMapping(value="/{id}")
+    public ResponseEntity deletarProduto(@PathVariable Long id){
+        return ResponseEntity.ok(produtoService.deletarProduto(id));
     }
 }
--- loja/src/main/java/com/madstore/loja/service/ProdutoService.java
+++ loja/src/main/java/com/madstore/loja/service/ProdutoService.java
@@ -19,4 +19,40 @@ public class ProdutoService {
         return produtosDTO;
     }
 
+    public Produto findById(Long id){
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    public ProdutoDTO adicionarProduto(ProdutoDTO produtoDTO){
+        Produto produto = new Produto();
+        produto.setNome(produtoDTO.getNome());
+        produto.setDescricao(produtoDTO.getDescricao());
+        produto.setTamanho(produtoDTO.getTamanho());
+        produto.setValor(produto.getValor());
+
+        Produto novoProduto = produtoRepository.save(produto);
+
+        return new ProdutoDTO(novoProduto);
+    }
+
+    public ProdutoDTO atualizarProduto(Long produtoId, ProdutoDTO produtoAtualizado){
+        Produto produto = produtoRepository.findById(produtoId).orElse(null);
+        assert produto != null;
+        produto.setNome(produtoAtualizado.getNome());
+        produto.setDescricao(produtoAtualizado.getDescricao());
+        produto.setTamanho(produtoAtualizado.getTamanho());
+        produto.setValor(produtoAtualizado.getValor());
+        produtoRepository.save(produto);
+        return new ProdutoDTO(produto);
+    }
+
+    public boolean deletarProduto(Long id) {
+        if (produtoRepository.existsById(id)) {
+            produtoRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+
 }
